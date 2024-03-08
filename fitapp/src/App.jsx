0252ca7a32deb0e455ad5d7f80cbf8d5dd8d756d
mbelmontes7 <you@example.com>
2024-03-08 01:51:43 -0600

    Refactor App and Header components

--- fitapp/src/App.jsx
+++ fitapp/src/App.jsx
@@ -4,9 +4,7 @@ import Header from "./Header"
 
    function App() {
       return (
-        <h1 className="text-3xl font-bold underline text-red-300">
-          Hello world!
-        </h1>
+          <Header />
       )
     }
   
--- fitapp/src/Header.jsx
+++ fitapp/src/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+// src/components/Header.js
+import React from 'react';
 
-function Header() {
+const Header = () => {
   return (
-    <div class='text-red-900 text-8xl'>Header</div>
-  )
-}
+    <header className=" bg-red-500 text-white p-4 rounded-xl">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-1xl font-bold">Become Stronger 💪🏻</h1>
+        <nav>
+          <ul className="flex space-x-2">
+            <li><a href="#" className="hover:underline">Home</a></li>
+            <li><a href="#" className="hover:underline">Workouts</a></li>
+            <li><a href="#" className="hover:underline">Nutrition</a></li>
+            <li><a href="#" className="hover:underline">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
